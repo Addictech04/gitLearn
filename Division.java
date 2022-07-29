8523475a0d341a899833b8af6551b8909fe9963c
Nilay Kumar Paul <nilay.paul100@gmail.com>
2022-07-30 02:27:56 +0530

    Modified Subtract.java and Initial commit of Division.java

--- Division.java
+++ Division.java
@@ -1,8 +1,8 @@
-public class Subtract {
+public class Division {
 	public static void main(String[] args)
 	{
 		int a = 10, b = 5;
-		int result = a - b;
+		int result = a / b;
 		System.out.print("Result is: " + result);
 	}
 }
